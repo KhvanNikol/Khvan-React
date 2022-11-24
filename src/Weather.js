9f1db392c5ee9b98a1d547770b819ae8c2dcacb9
KhvanNikol <109013298+KhvanNikol@users.noreply.github.com>
2022-11-24 16:22:02 +0900

    Initial commit

---
+++ src/Weather.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import axios from "axios";
+export default function Weather(props) {
+  let [temperature, setTemperature] = useState(null);
+  function showTemperature(response) {
+    setTemperature(response.data.main.temp);
+  }
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=3a94f3778290bfeee61278505dbbe51d&units=metric`;
+  axios.get(url).then(showTemperature);
+
+  return (
+    <h1>
+      The temperature in {props.city} is {Math.round(temperature)}℃
+    </h1>
+  );
+}
